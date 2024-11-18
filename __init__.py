42611dca2fef5ae82860485d6c82749445fba765
dorhs <152424599+dorhs@users.noreply.github.com>
2024-11-18 15:05:33 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from .routes import main  # ייבוא של ה-Blueprint
-
-def create_app():
-    app = Flask(__name__)  # יצירת אפליקציה חדשה
-    app.register_blueprint(main)  # רישום ה-Blueprint של הנתיבים
-    return app
