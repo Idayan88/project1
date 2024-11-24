f07f698a5a70793e6300241f8aff5d7cad0ff70b
dor <youremail@example.com>
2024-11-24 18:50:46 +0200

    initial commit

--- app/main.py
+++
@@ -1,7 +0,0 @@
-from app import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    print(app.url_map)  # הדפסת כל הנתיבים המוגדרים באפליקציה
-    app.run(debug=True)  # הפעלת השרת על פורט 5000
