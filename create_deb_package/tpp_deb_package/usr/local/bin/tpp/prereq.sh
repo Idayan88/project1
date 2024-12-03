8eb3f44e22f181d65e26bcbcabc5a218aaf3949d
Denis-Golkov <dgolkov1987@gmail.com>
2024-12-03 21:59:57 +0200

    adding...files..for..automation1.1.1

--- create_deb_package/tpp_deb_package/usr/local/bin/tpp/prereq.sh
+++ create_deb_package/tpp_deb_package/usr/local/bin/tpp/prereq.sh
@@ -1,6 +1,9 @@
+
 if [ ! -f /etc/tpp/control ]; then
     sudo apt -y update
     sudo apt -y install python3 python3-pip
-    sudo pip3 install -r /etc/tpp/requirements.txt --break-system-packages # use --break-system-packages to install packages globally    
+    python3 -m venv venv
+    source venv/bin/activate
+    sudo pip3 install -r /etc/tpp/requirements.txt 
     sudo touch /etc/tpp/control
 fi
--- create_deb_package/tpp_deb_package/usr/local/bin/tpp_start.sh
+++ create_deb_package/tpp_deb_package/usr/local/bin/tpp_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Starting tpp_app..."
 sh /usr/local/bin/tpp/prereq.sh
+source /etc/tpp/venv/bin/activate
 python3 /usr/local/bin/tpp/app.py
 
