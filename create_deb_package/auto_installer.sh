87e832c0d56b367255c26339c8c6bd1580e69f6f
Denis-Golkov <dgolkov1987@gmail.com>
2024-12-03 19:09:18 +0200

    adding...files..for..automation1.0.1

--- create_deb_package/auto_installer.sh
+++ create_deb_package/auto_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone -b Denis_Liveness_ssl https://github.com/Idayan88/project1.git
-cd int0924/system/create_deb_package
-dpkg-deb --build systeminfo_deb_package
-mv systeminfo_deb_package.deb systeminfo.deb
-echo "systeminfo.deb package is ready."
-sudo apt -y install ./systeminfo.deb
+cd /home/ubuntu/project1/create_deb_package
+dpkg-deb --build tpp_deb_package
+mv tpp_deb_package.deb tpp.deb
+echo "tpp.deb package is ready."
+sudo apt -y install ./tpp.deb
