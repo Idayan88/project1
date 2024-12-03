26fdf2b4f612ee50b349d904b495a0a67ca0f970
Denis-Golkov <dgolkov1987@gmail.com>
2024-12-03 19:21:34 +0200

    adding...files..for..automation1.0.2

--- create_deb_package/auto_installer.sh
+++ create_deb_package/auto_installer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 git clone -b Denis_Liveness_ssl https://github.com/Idayan88/project1.git
+echo "The clone op Done"
 cd /home/ubuntu/project1/create_deb_package
+echo "cd done"
 dpkg-deb --build tpp_deb_package
 mv tpp_deb_package.deb tpp.deb
 echo "tpp.deb package is ready."
